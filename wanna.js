8545b7c9499e4e6e72e4dd86967816df1c42389e
Val Saven <val.saven@gmail.com>
2017-11-18 03:09:10 +0300

    Fix Instagram downloads
    Update parser for the latest Instagram client changes

--- wanna.js
+++ wanna.js
@@ -6,13 +6,13 @@ chrome.browserAction.onClicked.addListener(function (tab) {
       chrome.tabs.executeScript({
         code: `document.querySelectorAll('article > div > div > div > div:nth-child(1) > img:nth-child(1)')[0].src;`
       }, getInstagram);
-    } else {
+    } else if (tab.url.includes('stories')) {
       chrome.tabs.executeScript({ // Stories Photo
-        code: `document.querySelectorAll('div.ReactModalPortal > div > div > div > div.inner.ril-inner > img')[0].src;`
+        code: `document.querySelectorAll('#react-root > section > div > div > section > div > div > div > div > img')[0].src;`
       }, getInstagram);
 
       chrome.tabs.executeScript({ // Stories Video
-        code: `document.querySelectorAll('div.ReactModalPortal > div > div > div > div.inner.ril-inner > div.image-current.ril-image-current > div > video')[0].src;`
+        code: `document.querySelectorAll('video > source')[0].src;`
       }, getInstagram);
     }
   } else {
