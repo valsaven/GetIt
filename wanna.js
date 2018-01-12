ec5582c773393352922e7254c04cd33e8e299827
Val Saven <val.saven@gmail.com>
2018-01-12 04:00:56 +0300

    Add z0r.de support and refactor

--- wanna.js
+++ wanna.js
@@ -1,32 +1,41 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
-  console.log(tab.url);
+  console.log(tab.url); // TODO: Remove
 
   if (tab.url.includes('instagram')) {
     if (tab.url.includes('/p/')) { // Photo
       chrome.tabs.executeScript({
-        code: `document.querySelectorAll('article > div > div > div > div:nth-child(1) > img:nth-child(1)')[0].src;`
-      }, getInstagram);
+        code: `document.querySelector('article > div > div > div > div:nth-child(1) > img:nth-child(1)').src;`
+      }, getFile);
     } else if (tab.url.includes('stories')) {
       chrome.tabs.executeScript({ // Stories Photo
-        code: `document.querySelectorAll('#react-root > section > div > div > section > div > div > div > div > img')[0].src;`
-      }, getInstagram);
+        code: `document.querySelector('#react-root > section > div > div > section > div > div > div > div > img').src;`
+      }, getFile);
 
       chrome.tabs.executeScript({ // Stories Video
-        code: `document.querySelectorAll('video > source')[0].src;`
-      }, getInstagram);
+        code: `document.querySelector('video > source').src;`
+      }, getFile);
     }
+  } if (tab.url.includes('z0r.de')) {
+    console.log('z0r');
+    chrome.tabs.executeScript({
+      code: `document.querySelector('body > object').data;`
+    }, getFile);
   } else {
     console.log('Nothing to download...');
   }
 });
 
-function getInstagram(arr) {
+function getFile(arr) {
   var url = arr[0];
+  if (!url) {
+    return false;
+  }
+
   try {
     chrome.downloads.download({
       url
     });
   } catch (e) {
-    console.log(e);
+    console.log('Error(getFile): ' + e);
   }
 }
