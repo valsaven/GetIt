922d32d254cf0c396a35903abb97250cf76c50b9
Val Saven <val.saven@gmail.com>
2018-02-27 18:54:03 +0300

    Migrate: Webpack 3 => 4

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,38 +1,32 @@
 import webpack from 'webpack';
 import path from 'path';
 
-import CleanWebpackPlugin from "clean-webpack-plugin";
-import CopyWebpackPlugin from "copy-webpack-plugin";
+import CleanWebpackPlugin from 'clean-webpack-plugin';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
 
 const config = {
   entry: './src/wanna.js',
   output: {
     filename: 'wanna.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
-    rules: [{
-      test: /\.js$/,
-      loader: 'babel-loader',
-      exclude: /node_modules/
-    }]
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
-    new CopyWebpackPlugin([{
-      from: 'static'
-    }]),
-    new webpack.optimize.UglifyJsPlugin({
-      // sourceMap: true,
-      compress: {
-        warnings: false
-      }
-    }),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true
-    })
+    new CopyWebpackPlugin([
+      {
+        from: 'static',
+      },
+    ]),
   ],
-  devtool: '#source-map',
 };
 
 module.exports = config;
